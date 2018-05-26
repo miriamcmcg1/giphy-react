eb40ca3e4e3187f9981c58fbbd181a47e771358d
marga <miriamcristianamcg@gmail.com>
2018-05-26 21:20:07 +0100

    fixed lint errors

--- src/containers/MainPage.js
+++ src/containers/MainPage.js
@@ -2,28 +2,26 @@ import React, { Component } from "react";
 import request from "superagent";
 
 import Header from "../components/Header";
-import SampleStore from "../stores/SampleStore";
 import Constants from "../constants/AppConstants";
 import GridList from "@material-ui/core/GridList";
 import GridListTile from "@material-ui/core/GridListTile";
-import GridListTileBar from '@material-ui/core/GridListTileBar';
+import GridListTileBar from "@material-ui/core/GridListTileBar";
 import IconButton from "@material-ui/core/IconButton";
 import FavIcon from "@material-ui/icons/FavoriteBorder";
 
 const styles = {
   divStyle: {
-    display: 'flex',
-    flexWrap: 'wrap',
-    justifyContent: 'space-around',
-    overflow: 'hidden',
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "space-around",
+    overflow: "hidden"
   },
   gridListStyle: {
-    width: '100%',
-    // Promote the list into his own layer on Chrome. This cost memory but helps keeping high FPS.
+    width: "100%",
     transform: "translateZ(0)"
   },
   titleBarStyle: {
-    background: "linear-gradient(to bottom, rgba(0,0,0,0.7) 0%, " + "rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)"
+    background: "linear-gradient(to bottom, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)"
   },
   iconStyle: {
     color: "white"
@@ -40,13 +38,13 @@ class MainPage extends Component {
 
   getgifs() {
     let self = this;
-    let url = "http://api.giphy.com/v1/gifs/trending?api_key=" + Constants.API_KEY+ "&limit=24";
+    let url = `http://api.giphy.com/v1/gifs/trending?api_key=${Constants.API_KEY}&limit=24`;
 
     request
       .get(url)
       .accept("json")
       .end(function(err, response) {
-        if (response.body.meta.status == 200) {
+        if (response.body.meta.status === 200) {
           let gifs = response.body.data.map(gif => {
             return {
               url: gif.images.downsized.url,
@@ -70,16 +68,30 @@ class MainPage extends Component {
       <div>
         <Header title="GiphyLand" />
         <div style={styles.divStyle}>
-          <GridList  spacing={0} cols={4} style={styles.gridListStyle}>
+          <GridList spacing={0} cols={4} style={styles.gridListStyle}>
             {this.state.gifs.map((gif, idx) => (
-              <GridListTile onMouseEnter={() => {this.state.gifs[idx].titleVisible = true; this.setState({gifs: this.state.gifs})}} onMouseLeave={() => gif.titleVisible = false} key={gif.id} cols={gif.featured ? 2 : 1} rows={gif.featured ? 2 : 1}>
-                <img src={gif.url} alt={gif.title}/>
+              <GridListTile
+                onMouseEnter={() => {
+                  const gifs = this.state.gifs.slice();
+                  gifs[idx].titleVisible = true;
+                  this.setState({ gifs: gifs });
+                }}
+                onMouseLeave={() => {
+                  let gifs = this.state.gifs.slice();
+                  gifs[idx].titleVisible = false;
+                  this.setState({ gifs: gifs });
+                }}
+                key={gif.id}
+                cols={gif.featured ? 2 : 1}
+                rows={gif.featured ? 2 : 1}
+              >
+                <img src={gif.url} alt={gif.title} />
                 <GridListTileBar
                   title={gif.titleVisible ? gif.title : null}
                   titlePosition="top"
                   actionIcon={
                     <IconButton style={styles.iconStyle}>
-                      <FavIcon/>
+                      <FavIcon />
                     </IconButton>
                   }
                   actionPosition="left"
